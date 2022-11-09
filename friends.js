433ca20fd37bf1dfd0071cbcc54f0ccea4587e02
Kirk Hauck <kirkihauck@gmail.com>
2022-11-09 16:14:41 -0700

    Add array

--- friends.js
+++ friends.js
@@ -3,3 +3,7 @@ var names = ["Ben", "Kirk", "Joe", "Travis", "MysteryMan"]
 console.log(names[2])
 console.log(names[4])
 console.log(names[0])
+
+var places = ["Honk Kong", "Paris", "London", "Fiji", "Antarctica"]
+places.pop()
+console.log(places)
